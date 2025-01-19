b86f7f7bb825745be959e50fda61347a58b30145
Mohit <tihom7791@gmail.com>
2025-01-19 08:46:51 +0530

    commited total

--- api/index.js
+++ api/index.js
@@ -150,7 +150,7 @@ const instance = new Razorpay({
 app.get("/razor/order", (req, res) => {
   try {
     const options = {
-      amount: 20 * 100, // amount == Rs 10
+      amount: req.query.total * 100, // amount == Rs 10
       currency: "INR",
       receipt: "receipt#1",
       payment_capture: 1,
